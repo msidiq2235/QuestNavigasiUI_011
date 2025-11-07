2994c364b092c0c4de1b9dac518026bce7d25a85
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-07 15:18:10 +0700

    menambah field input OutlinedTextField 'Nama Lengkap'

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -39,5 +39,15 @@ fun FormIsian(
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+
         }
 }
