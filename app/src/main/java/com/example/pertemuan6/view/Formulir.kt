a321c7718c953f5c939ce63950ba1f574a92412d
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-07 15:22:50 +0700

    menambah layout awal untuk pilihan 'Jenis Kelamin'

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.pertemuan6.R
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -51,6 +53,17 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = Thickness, color = Color.Red)
+            Row {
+                JenisK.forEach {
+                    item->
+                    Row(verticalAlignment = Alignment.CenterHorizontally){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
             }
         }
 }
