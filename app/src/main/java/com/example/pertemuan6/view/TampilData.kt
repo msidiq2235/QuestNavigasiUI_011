f57b6d263c7ad5a4cb3e2c72876ecfaf9785a1d9
msidiq2235 <muhsidiq2005@gmail.com>
2025-11-07 16:26:40 +0700

    membuat Navigasi.kt

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -3,7 +3,6 @@ package com.example.pertemuan6.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
